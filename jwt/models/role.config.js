4535f902e16d8180f958878cf0e6e75f9204e365
pupil <pupil@ivkhk.ee>
2023-02-27 15:04:32 +0200

    jwt

---
+++ jwt/models/role.config.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sequelize) => {
+    const Role = sequelize.define("roles", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Role;
+  };
